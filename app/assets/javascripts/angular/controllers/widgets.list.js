ac57c08902c64bfc0b1f4ac3c0329fedc0a2c4b3
Stephen Bussey <steve.bussey@salesloft.com>
2014-10-05 00:44:53 -0400

    Adds in creation

--- app/assets/javascripts/angular/controllers/widgets.list.js
+++ app/assets/javascripts/angular/controllers/widgets.list.js
@@ -2,4 +2,10 @@ APP.controller('WidgetsListController', ['$scope', 'Restangular', function($scop
   Restangular.all("widgets").getList().then(function(widgets) {
     $scope.widgets = widgets;
   });
+
+  $scope.create = function(widget) {
+    Restangular.all("widgets").post(widget).then(function(widget) {
+      $scope.widgets.push(widget);
+    });
+  };
 }]);
